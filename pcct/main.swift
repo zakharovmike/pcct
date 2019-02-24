936a51450d7fb675869bf7946e26286e8dec3462
Mikhail Zakharov <mizak@envisage.no>
2019-02-24 01:46:44 +0100

    Initial Commit

---
+++ pcct/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  pcct
+//
+//  Created by Mikhail Zakharov on 2019-02-24.
+//  Copyright © 2019 Mikhail Zakharov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
